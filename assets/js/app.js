dd2d99730863022900afb8b4de8feb41c5d1544a
Vinny <espinalv1@gmail.com>
2016-10-20 17:21:06 -0400

    Changed font, added some responsive, fixed api calls

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,35 @@
+var headHTML = document.querySelector('head');
+var quote = document.getElementById('quote')
+var author = document.getElementById('author')
+
+// On initial page load, use JSONP to access data from quote API.
+// When 'New Quote' button is clicked, remove old script, insert new script.
+// Query string in URL includes function call.
 function apiScript() {
-  var bodyHTML = document.querySelector('body');
+  if (headHTML.hasChildNodes('script')) {
+    var lastChild = document.querySelector('head').lastChild;
+    headHTML.removeChild(lastChild);
+  }
+
   var script = document.createElement('script');
   script.src = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=parseJSONP"
-  bodyHTML.appendChild(script);
+  document.querySelector('head').appendChild(script);
 }
 apiScript();
 
+// Use data from the object passed by the JSONP call.
+// If author value is empty, use "Unknown".
 function parseJSONP(jsonp) {
-  document.getElementById('quote').innerHTML = jsonp.quoteText;
-  document.getElementById('author').innerHTML = jsonp.quoteAuthor;
+  quote.textContent = '“' + jsonp.quoteText + '”';
+  if (jsonp.quoteAuthor) {
+    author.textContent ="― " + jsonp.quoteAuthor;
+  } else {
+    author.textContent = "― Unknown";
+  };
+}
+
+// When link is clicked, set the href attribute to new link + quote content.
+function tweetQuote() {
+  var tweetQuote = document.getElementById('tweet-quote');
+  tweetQuote.setAttribute('href', 'https://twitter.com/intent/tweet?text=' + quote.textContent + " " + author.textContent);
 }
